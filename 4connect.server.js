ba0b5c67f92c9eaf644fda4078bd2b894afe55c0
Gilles <hellsdark@gmail.com>
2020-09-09 22:43:38 +0200

    allow n number of players simultaneously, not just two

--- 4connect.server.js
+++ 4connect.server.js
@@ -3,6 +3,7 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var path = require("path");
+var fn = require("./4connect.functions");
 
 app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, ".") + "/4connect.html");
@@ -31,173 +32,112 @@ const events = {
   },
 };
 
-var board = [];
-var colHeights = [];
-var player1 = false;
-var player2 = false;
-var currentPlayer = players.PLAYER_1;
+const games = new Map();
 
 io.on(events.IN.CONNECTION, (socket) => {
-  if (!player1) {
-    player1 = true;
+  game = fn.getAvailableGame(games);
+  if (!game) {
+    game = fn.createNewGame();
+    games.set(game.id, game);
+  }
+  socket.game = game;
+  socket.join(game.id);
+
+  if (!socket.game.player1) {
+    socket.game.player1 = true;
     socket.player = players.PLAYER_1;
-  } else if (!player2) {
-    player2 = true;
+  } else if (!socket.game.player2) {
+    socket.game.player2 = true;
     socket.player = players.PLAYER_2;
   } else {
     socket.emit(events.OUT.FORBIDDEN, "no_place_available");
     return;
   }
 
-  console.log("player " + socket.player + " logged in.");
+  console.log(
+    "player ",
+    socket.player,
+    " joined room",
+    game.id,
+    socket.game.player1,
+    socket.game.player2
+  );
+
   socket.emit(events.OUT.NUMBER, socket.player);
 
-  if (player1 && player2) {
-    console.log("ready");
-    resetBoard();
-    io.emit(events.OUT.STATUS, "ready");
-    console.log(board);
+  if (socket.game.player1 && socket.game.player2) {
+    console.log(socket.game.id, "ready");
+
+    fn.resetBoard(socket.game);
+    console.log(game.id, "emit board", game.board, game.currentPlayer);
+    io.in(game.id).emit(events.OUT.BOARD, {
+      board: game.board,
+      player: game.currentPlayer,
+    });
+
+    io.in(game.id).emit(events.OUT.STATUS, "ready");
+    console.log(socket.game.id, socket.game.board);
   }
 
   socket.on(events.IN.PLAY, function (x) {
-    console.log("played on " + x + "=>" + colHeights[x - 1]);
-    if (currentPlayer != socket.player || !moveEnabled(x)) {
-      console.log("wrong move : " + x);
+    console.log(
+      socket.game.id,
+      "played on " + x + "=>" + socket.game.colHeights[x - 1]
+    );
+    if (
+      socket.game.currentPlayer != socket.player ||
+      !fn.moveEnabled(socket.game, x)
+    ) {
+      console.log(socket.game.id, "wrong move : " + x);
       socket.emit(events.OUT.FORBIDDEN, "wrong_move");
     } else {
-      board[x - 1][colHeights[x - 1]] = socket.player;
-      colHeights[x - 1]++;
-      currentPlayer =
-        currentPlayer == players.PLAYER_1 ? players.PLAYER_2 : players.PLAYER_1;
-      console.log(board);
-      io.emit("board", { board: board, player: currentPlayer });
+      socket.game.board[x - 1][socket.game.colHeights[x - 1]] = socket.player;
+      socket.game.colHeights[x - 1]++;
+      socket.game.currentPlayer =
+        socket.game.currentPlayer == players.PLAYER_1
+          ? players.PLAYER_2
+          : players.PLAYER_1;
+      console.log(socket.game.board);
+      io.in(socket.game.id).emit("board", {
+        board: socket.game.board,
+        player: socket.game.currentPlayer,
+      });
     }
 
-    var winner = win();
+    var winner = fn.win(socket.game.board);
     if (winner) {
-      io.emit(events.OUT.GAMEOVER, winner);
-      console.log("player " + winner + " won !");
+      io.in(socket.game.id).emit(events.OUT.GAMEOVER, winner);
+      console.log(socket.game.id, "player " + winner + " won !");
     }
   });
 
   socket.on(events.IN.RESTART, function () {
-    resetBoard();
+    fn.resetBoard(socket.game);
+    io.in(socket.game.id).emit(events.OUT.BOARD, {
+      board: socket.game.board,
+      player: socket.game.currentPlayer,
+    });
   });
 
   socket.on(events.IN.DISCONNECT, function () {
-    console.log("player " + socket.player + " disconnected");
+    console.log(socket.game.id, "player " + socket.player + " disconnected");
     if (socket.player) {
       if (socket.player == players.PLAYER_1) {
-        player1 = false;
+        socket.game.player1 = false;
       } else if (socket.player == players.PLAYER_2) {
-        player2 = false;
+        socket.game.player2 = false;
       }
-      resetBoard();
-      io.emit(events.OUT.STATUS, "not-ready");
+
+      if (!socket.game.player1 && !socket.game.player2) {
+        console.log(socket.game.id, "end session");
+        games.delete(socket.game.id);
+      } else {
+        fn.resetBoard(socket.game);
+        io.in(game.id).emit(events.OUT.STATUS, "not-ready");
+      }
     }
   });
 });
-
-function resetBoard() {
-  board = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-  ];
-  colHeights = [0, 0, 0, 0, 0, 0, 0];
-  currentPlayer = players.PLAYER_1;
-  console.log("board reset");
-  io.emit(events.OUT.BOARD, { board: board, player: currentPlayer });
-}
-
-moveEnabled = (x) => x > 0 && x <= 7 && colHeights[x - 1] < 7;
-
-function win() {
-  return checkXAndYAxes() || checkDiagonals() || 0;
-}
-
-function checkXAndYAxes() {
-  var currentPlayerHori = 0;
-  var currentPlayerVerti = 0;
-  var cumulVerti = 0;
-  var cumulHori = 0;
-  for (var i = 0; i < 7; i++) {
-    for (var j = 0; j < 7; j++) {
-      // check columns
-      if (board[i][j] != currentPlayerVerti) {
-        cumulVerti = 0;
-        currentPlayerVerti = board[i][j];
-      }
-      if (currentPlayerVerti != 0) {
-        cumulVerti++;
-        if (cumulVerti == 4) {
-          return currentPlayerVerti;
-        }
-      }
-
-      // check lines
-      if (board[j][i] != currentPlayerHori) {
-        cumulHori = 0;
-        currentPlayerHori = board[j][i];
-      }
-      if (currentPlayerHori != 0) {
-        cumulHori++;
-        if (cumulHori == 4) {
-          return currentPlayerHori;
-        }
-      }
-    }
-    cumulHoriz = 0;
-    comulVerti = 0;
-    currentPlayerVerti = 0;
-    currentPlayerHori = 0;
-  }
-}
-
-function checkDiagonals() {
-  var currentPlayerDiag1 = 0;
-  var currentPlayerDiag2 = 0;
-  var cumulDiag1 = 0;
-  var cumulDiag2 = 0;
-  for (var k = 0; k < 6; k++) {
-    for (var l = 6; l >= 0; l--) {
-      // check diag 1
-      for (var i = k, j = l; i < 7 && j >= 0; i++, j--) {
-        if (board[i][j] != currentPlayerDiag1) {
-          cumulDiag1 = 0;
-          currentPlayerDiag1 = board[i][j];
-        }
-        if (currentPlayerDiag1 != 0) {
-          cumulDiag1++;
-          if (cumulDiag1 == 4) {
-            return currentPlayerDiag1;
-          }
-        }
-      }
-      //check diag 2
-      for (var i = k, j = l; i < 7 && j < 7; i++, j++) {
-        if (board[i][j] != currentPlayerDiag2) {
-          cumulDiag2 = 0;
-          currentPlayerDiag2 = board[i][j];
-        }
-        if (currentPlayerDiag2 != 0) {
-          cumulDiag2++;
-          if (cumulDiag2 == 4) {
-            return currentPlayerDiag2;
-          }
-        }
-      }
-      cumulDiag1 = 0;
-      comulDiag2 = 0;
-      currentPlayerDiag1 = 0;
-      currentPlayerDiag2 = 0;
-    }
-  }
-}
 
 let port = process.env.PORT || 8080;
 http.listen(port, function () {
